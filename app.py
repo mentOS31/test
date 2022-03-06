7d1f0ac33d3466654a17fa7e72d251934dd95ee4
mentOS31 <50947403+mentOS31@users.noreply.github.com>
2022-03-06 17:45:59 +0900

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,13 @@
 from flask import Flask, jsonify, abort
+from flask_restful import Api, Resource
 
 app = Flask(__name__)
 
 @app.route("/")
 def abort_():
-    abort(404)
+    return {}, 404
 
+@app.route("/POST/")
+    
 if __name__ == '__main__':
     app.run()
